3e9e8c3082deccc91b51761779928a10f4bdbf40
Tianchou Gong <tianchougong@gmail.com>
2022-06-30 21:15:06 -0400

    card.js, display.js, app.js, and index.js baisc code

---
+++ src/components/card.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Card(props){
+    const cardStyle = {
+        width:'15rem',
+    };
+    return (
+        <div className="container">
+          <div className="card" style={cardStyle}>
+            <img
+              className="card-img-top"
+              src={``}
+              alt="Card cap"
+            />
+            <div className="card-body">
+            <h5 className="card-title">Name: {props.name}</h5>
+              <p className="card-text">Price: {props.price}</p>
+              <a href="#" className="btn btn-primary">
+                Add to Cart {props.name}
+              </a>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    
