8b0f81ba7326b67994e6f6042d367bb0d4637248
Andrew Banagas <andrewbanagas40@gmail.com>
2022-07-03 15:46:55 -0700

    Updated src

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import Home from "./pages/Home";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-
-function App() {
-  return (
-    <>
-    <div>
-      <Header />
-    
-      <div>
-        <Home />
-      </div>
-      <Footer />
-    </div>
-    </>
-=======
 import Display from "./components/Display";
 import { Routes, Route, Link } from "react-router-dom";
 import Login from "./components/Page/Login";
@@ -29,7 +11,6 @@ function App() {
       <Route path="/" element={<Display />} />
     </Routes>
     // <Display />
->>>>>>> 767458b (Updated src)
   );
 }
 
