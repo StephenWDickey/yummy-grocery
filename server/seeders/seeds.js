c0948d354985966585a13554ef7665a308a9338f
Tianchou Gong <tianchougong@gmail.com>
2022-07-04 06:02:51 -0400

    fill out the products list

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -14,17 +14,84 @@ db.once('open', async () => {
 
     await db.collection('Product').insertMany([
         {
-            name: 'Eggs',
-            price: 5.00
+            name: 'Eggs - Cargo Free, 18 Each',
+            price: 5.00/ea
         },
         {
-            name: 'Milk',
-            price: 5.00
+            name: 'Milk - Far free, 0.5 Gallon',
+            price: 5.00/ea
         },
         {
-            name: 'Blueberries',
-            price: 3.00
-        }
+            name: 'Fresh Blueberries',
+            price: 3.00/lb
+        },
+        {
+            name:"Organic Bananas",
+            price:1.29/lb
+        },
+        {
+            name:"Fresh Mango, 1 Each",
+            price:0.9/ea,
+            was:1.59/ea
+        },
+        {
+            name:"Fresh Kiwifruit, 1 Each",
+            price:1.29/ea
+        },
+        {
+            name:"Large Pineaaple, 1 Each",
+            price:2.49/ea,
+            was:2.99/ea
+        },
+        {
+            name:"Tomatoes",
+            price:1.99/lb
+        },
+        {
+            name:"Patatoes",
+            price:1.99/lb
+        },
+        {
+            name:"Organic Giner",
+            price:1.49/lb
+        },
+        {
+            name:"Organic Scallons, 1 Each",
+            price:1.49/ea
+        },
+        {
+            name:"Yourt",
+            price:1.49/ea
+        },
+        {
+            name:"Bakey Bread, 20 Ounce",
+            price:2.49/ea,
+            was:3.99/ea
+        },
+        {
+            name:"Butter Croissants, 4ct",
+            price:5.99/ea
+        },
+        {
+            name:"Fresh Salmon, 1 Pound",
+            price:13.99/lb
+        },
+        {
+            name:"Tiger Shrimp",
+            price:8.99/lb
+        },
+        {
+            name:"Chicken Breasts, 5 Pound",
+            price:13.99/ea
+        },{
+            name:"Chicken Wings, 2.3 Pound",
+            price:8.59/ea
+        },{
+            name:"New York Strip Steak, 1.4 Pound",
+            price:20.99/ea
+        },
+        
+        
     ], console.log('Products seeded!'));
 
 
