e9589e63d4f8bc85852aaacd302627c3f6b02a3c
Tianchou Gong <tianchougong@gmail.com>
2022-07-04 06:18:03 -0400

    token

--- client/src/navigation/Navbar.js
+++ client/src/navigation/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
+  <a className="navbar-brand" href="#">Yuumy</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -13,12 +13,12 @@ const Navbar = () => {
         <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Features</a>
+        <a className="nav-link" href="#"></a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Pricing</a>
+        <a className="nav-link" href="#"></a>
       </li>
-      <li className="nav-item dropdown">
+      {/* <li className="nav-item dropdown">
         <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Dropdown link
         </a>
@@ -27,7 +27,7 @@ const Navbar = () => {
           <a className="dropdown-item" href="#">Another action</a>
           <a className="dropdown-item" href="#">Something else here</a>
         </div>
-      </li>
+      </li> */}
     </ul>
   </div>
 </nav>
