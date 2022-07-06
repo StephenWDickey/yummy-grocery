c47c5ebef46044ad6cc1fdaa185634565bc1d866
stevey d <stphndcky22@gmail.com>
2022-07-06 15:13:37 -0700

    fixed errors with page rendering

--- client/src/components/Page/Cart.js
+++ client/src/components/Page/Cart.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Card from "../Card";
 import { useQuery } from "@apollo/client";
 import { QUERY_CURRENT_ORDER } from "../../utils/queries";
@@ -22,6 +22,9 @@ const Cart = () => {
   // for lazyQuery
   const [getCheckout] = useLazyQuery(QUERY_CHECKOUT);
 
+  // we use global state data
+  const [ state, dispatch ] = useStoreContext();
+
 
   function submitCheckout() {
     const productIds = [];
@@ -36,20 +39,7 @@ const Cart = () => {
       variables: { products: productIds }
     });
 
-    // use useEffect hook to retrieve data from cache for cart
-    useEffect(() => {
-
-      async function getCart() {
-        const cart = await idbPromise('cart', 'get');
-        dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
-      };
     
-      // if cart is empty we will check if cart data has been cached
-      // then getCart() function will retrieve it
-      if (!state.cart.length) {
-        getCart();
-      }
-    }, [state.cart.length, dispatch]);
   }
 
 
