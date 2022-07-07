4c0556ed50871d9dda209631565feedae592d9ae
stevey d <stphndcky22@gmail.com>
2022-07-07 16:03:28 -0700

    fixed issue with signup form!

--- client/src/components/Page/Signup.js
+++ client/src/components/Page/Signup.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, {useState} from "react";
 import "../../App.css";
 import { MdOutlineKeyboardBackspace } from "react-icons/md";
 import { Link } from "react-router-dom";
 
+// import AuthService from utils/auth
+import Auth from '../../utils/auth';
+
+// import useMutation hook and ADD_USER Mutation
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../../utils/mutations';
+
+
 function Signup() {
+
+  const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+  
+  // useMutation hook returns our mutation code
+  // then we add error handling
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+
+  
+  // submit form (notice the async!)
+  const handleFormSubmit = async event => {
+    
+    event.preventDefault();
+
+    // use try/catch instead of promises to handle errors
+    try {
+      // execute addUser mutation and pass in variable data from form
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+      console.log(data);
+
+      // pass the token into .login method
+      // that we created in utils/auth
+      Auth.login(data.addUser.token)
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <div className="signup">
       <div className="container ">
@@ -13,16 +62,18 @@ function Signup() {
           </Link>
         </div>
         <h2>Fill in the Details Below</h2>
-        <form className="flex-display">
+        <form 
+          className="flex-display"
+          onSubmit={handleFormSubmit}>
           <label className="field field_v1">
             <input
-              // value={}
-              // onChange={}
               type="text"
               name="username"
               placeholder="username"
               className="field__input"
               required
+              onChange={handleChange}
+              value={formState.username}
             />
             <span className="field__label-wrap">
               <span className="field__label">Username</span>
@@ -37,6 +88,8 @@ function Signup() {
               placeholder="email"
               className="field__input"
               required
+              onChange={handleChange}
+              value={formState.email}
             />
             <span className="field__label-wrap">
               <span className="field__label">Email</span>
@@ -45,13 +98,13 @@ function Signup() {
 
           <label className="field field_v1">
             <input
-              // value={}
-              // onChange={}
               type="password"
               name="password"
               placeholder="password"
               className="field__input"
               required
+              onChange={handleChange}
+              value={formState.password}
             />
             <span className="field__label-wrap">
               <span className="field__label">Password</span>
