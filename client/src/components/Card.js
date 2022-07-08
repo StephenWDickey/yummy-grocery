763048720c0d852a5865c251ad8d0525b746caed
stevey d <stphndcky22@gmail.com>
2022-07-07 17:42:26 -0700

    working on Card component

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -14,6 +14,7 @@ export default function Card(props) {
     var productName = event.target.getAttribute("data-name");
     var productPrice = event.target.getAttribute("data-price");
     localStorage.setItem("currentCartCount", parseInt(localStorage.getItem("currentCartCount"))+ 1);
+    window.alert(`${productName} added to cart`);
     try {
       const { data } = await addProduct({
         variables: { orderId: orderId, productName: productName, quantity: 1, price:parseInt(productPrice) },
@@ -26,6 +27,12 @@ export default function Card(props) {
   const cardStyle = {
     width: "15rem",
   };
+
+  const removeItem = async (event) => {
+    event.preventDefault();
+    localStorage.removeItem("orderId")
+    localStorage.setItem("currentCartCount", parseInt(localStorage.getItem("currentCartCount"))- 1);
+  }
 
   console.log(props.image);
   return (
@@ -40,9 +47,14 @@ export default function Card(props) {
           <h5>Name: {props.name}</h5>
           <p >Price: {props.price}</p>
           <p className="card-quantity">Quantity: 1</p>
-          <a onClick={addToCart} href="/" className="btn btn-primary" data-name = {props.name} data-price = {props.price}>
+          <a onClick={addToCart} href="/" className="btn background-primary btn-primary" data-name = {props.name} data-price = {props.price}>
             Add to Cart
           </a>
+          {props.length && (
+            <a onClick={removeItem} className="btn background-danger btn-danger">
+              Remove from Cart
+            </a>
+          )}
         </div>
       </div>
     </div>
