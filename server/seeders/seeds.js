52cd98e79a1a2e375fb9ae8f20a0cc640cbdc6a1
Tianchou Gong <tianchougong@gmail.com>
2022-07-07 22:07:22 -0400

    image link update

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -14,33 +14,41 @@ db.once('open', async () => {
         
         {
             name: 'Eggs - Cargo Free, 18 Each',
-            price: 5.00
+            price: 5.00,
+            image:"https://images.pexels.com/photos/162712/egg-white-food-protein-162712.jpeg?auto=compress&cs=tinysrgb&w=1600"
         },
         {
             name: 'Milk - Fat free, 0.5 Gallon',
-            price: 5.00
+            price: 5.00,
+            image:"https://images.pexels.com/photos/248412/pexels-photo-248412.jpeg?auto=compress&cs=tinysrgb&w=1600"
+
         },
         {
             name: 'Fresh Blueberries',
             price: 3.00,
-            image: "blueberry.jpg"
+            image: "https://images.pexels.com/photos/2539170/pexels-photo-2539170.jpeg?auto=compress&cs=tinysrgb&w=1600"
         },
         {
             name:'Organic Bananas',
-            price:1.29
+            price:1.29,
+            image:"https://images.pexels.com/photos/2875814/pexels-photo-2875814.jpeg?auto=compress&cs=tinysrgb&w=1600"
+
         },
         {
             name:'Fresh Mango, 1 Each',
-            price:0.9
-            
+            price:0.9,
+            image:"https://images.pexels.com/photos/2294471/pexels-photo-2294471.jpeg?auto=compress&cs=tinysrgb&w=1600"   
         },
         {
             name:'Fresh Kiwifruit, 1 Each',
-            price:1.29
+            price:1.29,
+            image:"https://media.istockphoto.com/photos/whole-kiwi-fruit-and-half-kiwi-fruit-on-white-picture-id834807852?b=1&k=20&m=834807852&s=612x612&w=0&h=bQMRlUIrza9y-VrePiiTu8TiWMeiFI9h3dNxUWHM0GI="
         },
         {
-            name:'Large Pineaple, 1 Each',
-            price:2.49
+            name:'Large Pineapple, 1 Each',
+            price:2.49,
+            image:"https://media.istockphoto.com/photos/whole-pineapple-and-pineapple-slice-pineapple-with-leaves-isolate-on-picture-id1156645871?b=1&k=20&m=1156645871&s=612x612&w=0&h=naqUzYIcFf4wYmIFtvt1DZEVxjAG2Mk34vvC8mGLlyo="
+
         
         },
         {
